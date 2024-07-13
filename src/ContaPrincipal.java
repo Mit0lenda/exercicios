cae7cfa2587039ac93ffea3675fc8fe5ec2e7952
Nicollas De Oliveira <nideoliveirafreitas@gmail.com>
2024-07-13 02:16:31 -0300

    att

---
+++ src/ContaPrincipal.java
@@ -0,0 +1,9 @@
+import br.com.alura.exercicios.exercicios;
+
+public class ContaPrincipal {
+    public static void main(String[] args) {
+        exercicios conta1 = new exercicios();
+        conta1.setNome("Nicollas");
+        System.out.println("Titular da conta:" + conta1.nome);
+    }
+}
